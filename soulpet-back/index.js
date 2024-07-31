bdf58c40495bf5f743d71d68b5e63e1ab65c6017
almir-soulcode <jose.almir@soulcode.com>
2024-07-31 10:00:38 -0300

    aula3107: introdução ao express; nodemon configurado

--- soulpet-back/index.js
+++ soulpet-back/index.js
@@ -2,6 +2,7 @@ import { connection, authenticate } from "./config/database.js";
 import { Cliente } from "./models/cliente.js";
 import { Endereco } from "./models/endereco.js";
 import { Pet } from "./models/pet.js";
+import express from "express";
 
 authenticate(connection).then(() => {
   // Após conectar no banco de dados, ele irá sincronizar os models
@@ -10,4 +11,19 @@ authenticate(connection).then(() => {
   // recomendado apenas durante o desenvolvimento
   //   connection.sync({ force: true });
   connection.sync();
+});
+
+// Definir a aplicação backend em Express
+// Recursos pré-configurados
+const app = express();
+
+// Definir os endpoints do backend
+// Métodos: GET (leitura), POST (inserção), PUT (alteração), DELETE (remoção)
+app.get("/hello", (req, res) => { // manipulador de rota
+  res.send("Batata!"); // enviando a resposta para quem solicitou
+});
+
+// Rodar a aplicação backend
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000/");
 });
