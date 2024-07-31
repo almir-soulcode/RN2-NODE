5a99985f77b76f8e3cf05fb6aeb75e145133f740
almir-soulcode <jose.almir@soulcode.com>
2024-07-31 14:10:53 -0300

    aula3107: inserção de clientes

--- soulpet-back/index.js
+++ soulpet-back/index.js
@@ -17,9 +17,13 @@ authenticate(connection).then(() => {
 // Recursos pré-configurados
 const app = express();
 
+// Garantir que todas as requisições que têm body sejam lidas como JSON
+app.use(express.json());
+
 // Definir os endpoints do backend
 // Métodos: GET (leitura), POST (inserção), PUT (alteração), DELETE (remoção)
-app.get("/hello", (req, res) => { // manipulador de rota
+app.get("/hello", (req, res) => {
+  // manipulador de rota
   res.send("Batata!"); // enviando a resposta para quem solicitou
 });
 
@@ -34,15 +38,33 @@ app.get("/clientes", async (req, res) => {
 // :id => parâmetro de rota
 app.get("/clientes/:id", async (req, res) => {
   // SELECT * FROM clientes WHERE id = 1;
-  const cliente = await Cliente.findOne({ 
+  const cliente = await Cliente.findOne({
     where: { id: req.params.id },
-    include: [Endereco] // juntar os dados do cliente com seu respectivo endereço
+    include: [Endereco], // juntar os dados do cliente com seu respectivo endereço
   });
-  
-  if(cliente) {
+
+  if (cliente) {
     res.json(cliente);
   } else {
     res.status(404).json({ message: "Cliente não encontrado!" });
+  }
+});
+
+app.post("/clientes", async (req, res) => {
+  // Extraimos os dados do body que serão usados na inserção
+  const { nome, email, telefone, endereco } = req.body;
+
+  try {
+    // Tentativa de inserir o cliente
+    await Cliente.create(
+      {nome, email, telefone, endereco},
+      {include: [Endereco]}, // indicamos que o endereço será salvo e associado ao cliente
+    );
+    res.json({ message: "Cliente criado com sucesso." });
+  } catch(err) {
+    // 500 -> Internal Error
+    console.log(err);
+    res.status(500).json({message: "Um erro ocorreu ao inserir cliente."});
   }
 });
 
